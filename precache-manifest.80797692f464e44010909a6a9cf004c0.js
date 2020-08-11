a025c17dd33f4491e30f488bff9d604e9b634e0f
sageconsole <test01sage@gmail.com>
2020-08-11 18:07:06 +0800

    Updates

---
+++ precache-manifest.80797692f464e44010909a6a9cf004c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e996c45c03c440210c356bd447b81302",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5abf76fd62a750287050",
+    "url": "/static/css/main.e9cd4286.chunk.css"
+  },
+  {
+    "revision": "5b6a4f796d3baa0c9d49",
+    "url": "/static/js/2.74894ab1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.74894ab1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5abf76fd62a750287050",
+    "url": "/static/js/main.b9bdddfd.chunk.js"
+  },
+  {
+    "revision": "4448e3ccf4fcf51cbc5b",
+    "url": "/static/js/runtime-main.d4584520.js"
+  }
+]);
