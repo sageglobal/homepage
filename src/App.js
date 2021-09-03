c23689244cf4432b55989869b96b1bc27c35459b
sageglobal <sage.global@msa.hinet.net>
2021-09-03 20:50:37 +0800

    20210903

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
 import React, { useState ,Component} from 'react'
+import marquee from './marquee'
 
 /////////////////////DATA FIELD//////////////////////////////////////////
-/*跑馬燈文字(不要用 ` 符號)*/
-const marquee=`Welcome to Sage's homepage. 八月精選優惠，AATCC摩擦附布。詳情來電洽詢，謝謝！ `;
-
 
 /*  ['中文','英文','簡介',[標準],[圖片位置]],*/
 //檢驗儀器設備
---
+++ src/marquee.js
@@ -0,0 +1,2 @@
+const mytext = `Welcome to Sage's homepage. 八月精選優惠，AATCC摩擦附布。詳情來電洽詢，謝謝！ .`;
+export default mytext ;
