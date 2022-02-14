655f539d289db54f24a92a971d471eb93f49643b
sageglobal <sage.global@msa.hinet.net>
2022-02-14 15:56:10 +0800

    20220214

--- src/marquee.js
+++ src/marquee.js
@@ -1,2 +1,2 @@
-const mytext = `Welcome to Sage's homepage. ISO Washing Machine(LBT-M8)全新上市，歡迎來電洽詢，謝謝 ！ `;
+const mytext = `Welcome to Sage's homepage. 虎福臨門迎新春，AATCC#10水洗附布新春優惠價，歡迎來電洽詢，謝謝 ！ `;
 export default mytext ;
