e36a39ac2b30b34e180eac9269c19c01f09cf47d
sageglobal <sage.global@msa.hinet.net>
2022-03-16 14:13:14 +0800

    20220316

--- src/marquee.js
+++ src/marquee.js
@@ -1,2 +1,2 @@
-const mytext = `Welcome to Sage's homepage. 虎福臨門迎新春，AATCC#10水洗附布新春優惠價，歡迎來電洽詢，謝謝 ！ `;
+const mytext = `Welcome to Sage's homepage. 3月SDC黃化試驗組優惠價，歡迎來電洽詢，謝謝 ！ `;
 export default mytext ;
