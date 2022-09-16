8cfc9ac824b8c506a3d4a8460038ae0f0465ceb9
sageglobal <sage.global@msa.hinet.net>
2022-09-16 17:58:26 +0800

    20220916

--- src/marquee.js
+++ src/marquee.js
@@ -1,2 +1,2 @@
-const mytext = `Welcome to Sage's homepage. ISO洗衣機(M8)全面上市特價中，歡迎來電洽詢，謝謝 ！ `;
+const mytext = `Welcome to Sage's homepage. 9月ISO DW水洗附布特惠價！ ISO洗衣機(M8)全面上市特價中，歡迎來電洽詢！ `;
 export default mytext ;
