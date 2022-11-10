11e8cb70e7f14a9ffd6646be98c2e4a9e4ac8934
sageglobal <sage.global@msa.hinet.net>
2022-11-10 15:24:14 +0800

    20221110

--- src/marquee.js
+++ src/marquee.js
@@ -1,2 +1,2 @@
-const mytext = `Welcome to Sage's homepage. 9月ISO DW水洗附布特惠價！ ISO洗衣機(M8)全面上市特價中，歡迎來電洽詢！ `;
+const mytext = `Welcome to Sage's homepage. 11月起毬試驗機軟木墊超激優惠價，歡迎來電洽詢！ `;
 export default mytext ;
