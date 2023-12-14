08edb23a038b00d1d1648ef16ea8773565f67619
sageglobal <sage.global@msa.hinet.net>
2023-12-14 17:50:45 +0800

    20231214

--- src/marquee.js
+++ src/marquee.js
@@ -1,2 +1,2 @@
-const mytext = `感謝，全國訂購AATCC M6洗衣機／福懋興業訂購ISO M8洗衣機、OPTI-DRY乾衣機 / 三芳化工訂購AATCC M6D乾衣機`;
+const mytext = `感謝，宏遠興業訂購WASCATOR FOM71CLS／全國訂購AATCC M6洗衣機／福懋興業訂購ISO M8洗衣機、OPTI-DRY乾衣機 / 三芳化工訂購AATCC M6D乾衣機`;
 export default mytext ;
