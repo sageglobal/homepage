e97a0324ae424f771444c7d399bfde528800adac
unknown <andrew140023@gmail.com>
2024-05-09 02:42:04 +0800

    20240509

--- src/marquee.js
+++ src/marquee.js
@@ -1,2 +1,2 @@
-const mytext = `感謝，台灣百和訂購AATCC M6洗衣機／宏遠興業訂購WASCATOR FOM71CLS／全國訂購AATCC M6洗衣機／福懋興業訂購ISO M8洗衣機、OPTI-DRY乾衣機.`;
+const mytext = `感謝，瑞泰纖維訂購MIELE PWM908洗衣機／越南日鼎訂購OPTI-DRY 烘乾機／台灣百和訂購AATCC M6洗衣機／宏遠興業訂購WASCATOR FOM71CLS／全國訂購AATCC M6洗衣機.`;
 export default mytext ;
