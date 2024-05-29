60592a535eb1ce4ecbd6674c1ed4b70138a62057
SageGlobal <sage.global@msa.hinet.net>
2024-05-29 17:44:08 +0800

    20240529

--- src/marquee.js
+++ src/marquee.js
@@ -1,2 +1,2 @@
-const mytext = `感謝，遠東新世紀訂購ISO M8洗衣機／瑞泰纖維訂購MIELE PWM908洗衣機／越南日鼎訂購OPTI-DRY 烘乾機／宏遠興業訂購WASCATOR FOM71 CLS／全國訂購AATCC M6洗衣機.`;
+const mytext = `感謝，遠東新世紀訂購ISO M8洗衣機／瑞泰纖維訂購MIELE PWM908洗衣機／越南日鼎訂購OPTI-DRY 烘乾機／宏遠興業訂購WASCATOR FOM71 CLS／全國訂購AATCC M6洗衣機..`;
 export default mytext ;
