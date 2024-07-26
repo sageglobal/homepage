57abb2e8bbd24cb1b02f093ddc724fb2afd983f2
SageGlobal <sage.global@msa.hinet.net>
2024-07-26 17:37:59 +0800

    20240726

--- src/marquee.js
+++ src/marquee.js
@@ -1,2 +1,2 @@
-const mytext = `感謝，宏良國際訂購OPTI-DRY烘乾機 & WASCATOR FOM71 CLS／遠東新世紀訂購ISO M8洗衣機／瑞泰纖維訂購MIELE PWM908洗衣機／全國訂購AATCC M6洗衣機.`;
+const mytext = `感謝，全業(越南)訂購LBT-M6D乾衣機2台 / 宏良國際訂購OPTI-DRY烘乾機 & WASCATOR FOM71 CLS／遠東新世紀訂購ISO M8洗衣機／瑞泰纖維訂購MIELE PWM908洗衣機／全國訂購AATCC M6洗衣機.`;
 export default mytext ;
