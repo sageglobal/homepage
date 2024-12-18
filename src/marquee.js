ea6523dc7a542f6d041ff1539d5d1ceeae4aef97
SageGlobal <sage.global@msa.hinet.net>
2024-12-18 17:55:57 +0800

    20241218

--- src/marquee.js
+++ src/marquee.js
@@ -1,2 +1,2 @@
-const mytext = `感謝，全業(越南)訂購LBT-M6D乾衣機2台 / 宏良國際訂購OPTI-DRY烘乾機 & WASCATOR FOM71 CLS／遠東新世紀訂購ISO M8洗衣機／瑞泰纖維訂購MIELE PWM908洗衣機／全國訂購AATCC M6洗衣機.`;
+const mytext = `感謝，發發紡織訂購實驗室全套設備 / YKK訂購ISO M8洗衣機 & WASHTEC水洗牢度機 / 立泰特殊印刷訂購ISO M8洗衣機 / 宏良國際訂購OPTI-DRY烘乾機 & WASCATOR FOM71 CLS.`;
 export default mytext ;
